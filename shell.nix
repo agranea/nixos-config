809cbd18f2b29c1d91b4f0fa174f187039a0931c
agranea <agranea@outlook.de>
2021-05-29 22:10:57 +0000

    add opam2nix, add cachix losercache

--- shell.nix
+++ shell.nix
@@ -2,10 +2,12 @@
 let 
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  opam2nix = import sources.opam2nix {};
 in 
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ ansible gnumake terraform terragrunt packer fd nixfmt niv lorri direnv tflint ];
+    nativeBuildInputs = with pkgs; [ cachix ansible gnumake terraform terragrunt packer fd nixfmt niv lorri direnv tflint ripgrep opam2nix ];
     shellHook = ''
         export MACHINE="tjens"
+        cachix use losercache
     '';
 }
