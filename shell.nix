0c85ee020a6d7df20032d3d96c300ce47d19e3b6
agranea <agranea@outlook.de>
2021-05-30 06:41:31 +0000

    cache to dockerfile, create shell2cache

--- shell.nix
+++ shell.nix
@@ -1,13 +1,17 @@
 { ... }:
 let 
+  cache_name = "losercache";
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
   opam2nix = import sources.opam2nix {};
+  shell2cache = pkgs.writeScriptBin "shell2cache" ''
+    nix-store --query --references $(nix-instantiate shell.nix) | xargs nix-store --realise | xargs nix-store --query --requisites | cachix push ${cache_name}
+  '';
 in 
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ cachix ansible gnumake terraform terragrunt packer fd nixfmt niv lorri direnv tflint ripgrep opam2nix ];
+    nativeBuildInputs = with pkgs; [ cachix ansible gnumake terraform terragrunt packer fd nixfmt niv lorri direnv tflint ripgrep opam2nix shell2cache ];
     shellHook = ''
         export MACHINE="tjens"
-        cachix use losercache
+        cachix use ${cache_name}
     '';
 }
