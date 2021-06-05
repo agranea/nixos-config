08cf74b63d7b678253f23c9c3be48516bee26eea
agranea <agranea@outlook.de>
2021-06-05 08:35:11 +0000

    functionize

--- nix/yarn-ex.nix
+++ nix/yarn-ex.nix
@@ -1,4 +1,15 @@
 { pkgs, ... }:
+let
+  dist-for-pkg = name: ''
+    mv $out/libexec/${name}/deps/${name}/dist $out
+    rm -rf $out/libexec
+    rm -rf $out/bin
+    pushd $out
+    ${pkgs.zip}/bin/zip release.zip dist/
+    rm -rf $out/dist
+    popd
+  '';
+in
 pkgs.yarn2nix-moretea.mkYarnWorkspace {
   src = ../pkgs/yarn-ex;
   buildPhase = ''
@@ -6,15 +17,7 @@ pkgs.yarn2nix-moretea.mkYarnWorkspace {
   '';
   packageOverrides = {
     agra-friend = {
-      distPhase = ''
-        mv $out/libexec/@agra/friend/deps/@agra/friend/dist $out
-        rm -rf $out/libexec
-        rm -rf $out/bin
-        pushd $out
-        ${pkgs.zip}/bin/zip release.zip dist/
-        rm -rf $out/dist
-        popd
-      '';
+      distPhase = dist-for-pkg "@agra/friend";
     };
   };
 }
